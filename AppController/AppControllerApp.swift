ce695fc4f11acd05f59d718a51a8fd880c1b10cd
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-12 16:00:13 +0700

    Initial Commit

---
+++ AppController/AppControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppControllerApp.swift
+//  AppController
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
